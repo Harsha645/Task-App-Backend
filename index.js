356824bca29044e69c45e60ae03b57c73153d1f0
Harsha645 <harshapramod19980124@gmail.com>
2023-05-01 22:45:17 +0530

    app config

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hii");
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(()=> {
+    console.log(`Task app is listen ${PORT}`);
+})
