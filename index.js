2cd2c66e9e7487157aa23305e711f0c967741848
Harsha645 <harshapramod19980124@gmail.com>
2023-05-04 22:40:51 +0530

    install & config cors

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("./db/dbConfig")
 
+const cors = require('cors')
+app.use(cors())
+
 const taskRout = require('./routs/TaskRouts')
 
 app.use('/',taskRout)
