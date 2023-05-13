57a5d52f665e393cda66f7563e8cc64dc10f58e1
Harsha645 <harshapramod19980124@gmail.com>
2023-05-14 00:34:09 +0530

    change Update method

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -4,7 +4,7 @@ const taskSchema = require('../model/TaskSchema')
 const getTask = async (req, res) => {
         try {
         const tasks = await taskSchema.find();
-        console.log(tasks);
+        //console.log(tasks);
         res.send(tasks);
     } catch (error) {
         res.status(500).json({ msg: error })
@@ -50,6 +50,7 @@ const updateTask = async (req, res) => {
                 (updateTask._id, { title: updateTask.title, description: updateTask.description, dueDate: updateTask.dueDate, status: updateTask.status })
             res.status(200).json({ msg: "Task Updated Succesfully" })
         } else {
+            console.log("update not found");
             res.status(404).json({ msg: "Task not found..!" })
         }
     } catch (error) {
